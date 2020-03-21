639811737ff7e4bd70bbf5bdd61a4b76efae0563
robbie <robbiekruszynski@gmail.com>
2020-03-21 14:02:36 -0700

    add types rs

---
+++ rust-basics/src/types.rs
@@ -0,0 +1,11 @@
+//Primtive Types --
+// Integers: u8, i8, i16, u64, u128, i128 (number of bits they take in memory) 
+// Floats: f32, f64
+// Boolean (bool)
+// Characters (char)
+// Tuples
+// Arrays
+
+pub fun run () {
+
+}
